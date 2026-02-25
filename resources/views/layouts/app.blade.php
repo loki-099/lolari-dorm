4fa116c3996d34752fd70932774927577b09dfec
unknown <s.mngabinete@usm.edu.ph>
2026-02-25 17:01:17 +0800

    dashboard setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <meta name="description" content="Lolari Dorm Management System">
+    <meta name="author" content="Themesberg">
+    <title>@yield('title', 'Dashboard')</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://flowbite-admin-dashboard.vercel.app//app.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://flowbite-admin-dashboard.vercel.app/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://flowbite-admin-dashboard.vercel.app/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://flowbite-admin-dashboard.vercel.app/favicon-16x16.png">
+    <link rel="icon" type="image/png" href="https://flowbite-admin-dashboard.vercel.app/favicon.ico">
+    <link rel="manifest" href="https://flowbite-admin-dashboard.vercel.app/site.webmanifest">
+    <link rel="mask-icon" href="https://flowbite-admin-dashboard.vercel.app/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@">
+    <meta name="twitter:creator" content="@">
+    <meta name="twitter:title" content="@yield('twitter-title', 'Lolari Dorm - Management Systen')">
+    <meta name="twitter:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">
+    <meta name="twitter:image" content="https://flowbite-admin-dashboard.vercel.app/">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="https://flowbite-admin-dashboard.vercel.app/">
+    <meta property="og:title" content="@yield('og-title', 'Lolari Dorm - Management System')">
+    <meta property="og:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://flowbite-admin-dashboard.vercel.app/images/og-image.png">
+    <meta property="og:image:type" content="image/png">
 
     <script>
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
         }
     </script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="bg-gray-50 dark:bg-gray-800">
     <main>
         @yield('main-content')
     </main>
