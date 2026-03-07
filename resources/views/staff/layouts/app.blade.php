31b2b8b7393de4b83e5bbbf1f85670b90dbc450d
RalphJade <ralphjadeomega@gmail.com>
2026-03-07 17:39:37 +0800

    Light and Dark Mode Fix

--- resources/views/staff/layouts/app.blade.php
+++ resources/views/staff/layouts/app.blade.php
@@ -61,7 +61,12 @@
 
                 <div class="my-2 border-t border-gray-200 dark:border-gray-700"></div>
 
-                <a href="{{ route('staff.reports.index') }}" class="flex items-center gap-3 px-4 py-2.5 text-sm font-medium text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
+                <a href="{{ route('staff.reports.index') }}" class="flex items-center gap-3 px-4 py-2.5 text-sm font-medium rounded-lg transition-colors 
+   @if(request()->routeIs('staff.reports.*')) 
+      bg-blue-50 dark:bg-blue-900/30 text-blue-600 dark:text-blue-400 
+   @else 
+      text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 
+   @endif">
                     <svg class="w-5 h-5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20"><path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path></svg>
                     <span>Reports</span>
                 </a>
@@ -121,9 +126,10 @@
                                 }
                             ">
                         <!-- Sun Icon (shown in dark mode) -->
-                        <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l-1.414-1.414a2 2 0 10-2.828 0l-1.414 1.414a1 1 0 101.414 1.414l1-1a2 2 0 012.828 0l1 1a1 1 0 101.414-1.414zM3 10a1 1 0 01-1-1V7a1 1 0 012 0v2a1 1 0 01-1 1zm14 0a1 1 0 011-1v-2a1 1 0 11-2 0v2a1 1 0 011 1zm0 3a1 1 0 01-1-1v-2a1 1 0 112 0v2a1 1 0 01-1 1z" clip-rule="evenodd"></path>
+                        <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden dark:block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clip-rule="evenodd"><path/>
                         </svg>
+
                         <!-- Moon Icon (shown in light mode) -->
                         <svg id="theme-toggle-dark-icon" class="w-5 h-5 dark:hidden" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
