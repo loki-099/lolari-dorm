b86d04813bb465f7be3b995100e95797ca6e45cd
loki-099 <loidatolentino358@gmail.com>
2026-03-10 01:22:25 +0800

    upcoming rent function

--- resources/views/layouts/boarder.blade.php
+++ resources/views/layouts/boarder.blade.php
@@ -112,7 +112,7 @@
             </a>
             <ul class="space-y-2 font-medium pt-4">
                 <li>
-                    <a href="{{ route('boarder.sample') }}"
+                    <a href="{{ route('boarder.dashboard') }}"
                         class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
                         <svg class="w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
